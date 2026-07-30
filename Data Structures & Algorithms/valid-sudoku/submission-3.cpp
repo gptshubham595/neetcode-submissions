553e0f830e8c22f41016a9c73709b02936356790
Shubham Kumar Gupta <24877361+gptshubham595@users.noreply.github.com>
2026-07-30 21:57:19 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& b) {
+        int r[9][9]={0};
+        int c[9][9]={0};
+        int box[3][3][9]={0};
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(b[i][j]=='.') continue;
+                int n= b[i][j]-'1';
+                if(r[i][n]++ || c[j][n]++ || box[i/3][j/3][n]++)
+                    return false; 
+            }
+        }
+
+        return 1;
+      
+    }
+};
