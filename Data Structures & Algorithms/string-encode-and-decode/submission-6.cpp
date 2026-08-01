6a4558a0a8f708fccc11947760336c9fbd0d583b
Shubham Kumar Gupta <24877361+gptshubham595@users.noreply.github.com>
2026-08-01 16:51:17 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& s) {
+        string ans="";
+        for(auto &i:s){
+            ans+=to_string(i.size())+'#'+i;
+        }
+        // cout<<ans<<endl;
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        for(int i=0;i<s.size();){
+            int j=s.find('#',i);
+            // cout<<j<<";";
+            string n=s.substr(i,j);
+            // cout<<n<<",";
+            int l=stoi(n);
+            // cout<<l;
+            ans.push_back(s.substr(j+1,l));
+            i=j+l+1;
+        }
+        return ans;
+    }
+};
